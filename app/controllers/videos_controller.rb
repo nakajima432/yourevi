2b1cd552dd9b5957bfb2010bd948fd85994566ea
takumi <akaranki4@gmail.com>
2020-05-07 12:18:30 +0900

    Revert "新規投稿ページの設定"
    This reverts commit 2809138e545b04dbb1b04929aca132280ab89f6a.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,15 +4,5 @@ class VideosController < ApplicationController
   end
 
   def new
-    @video =Video.new
-  end
-
-  def create
-    Video.create(video_params)
-  end
-
-  private
-  def video_params
-    params.require(:video).permit(:title, :content)
   end
 end
