10712f2413a49cb30c7af55181c82ca4701cc5f7
takumi <akaranki4@gmail.com>
2020-05-07 16:14:23 +0900

    videosコントローラーのshow,edit,update,destroyアクションの簡易的な設定

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,9 +12,25 @@ class VideosController < ApplicationController
     redirect_to videos_path
   end
 
-  # def show
-  #   @post = Post.find(params[:id])
-  # end
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    video = Video.find(params[:id])
+    video.update(video_params)
+    redirect_to video_path(video.id)
+  end
+
+  def destroy
+    video = Video.find(params[:id])
+    video.destroy
+    redirect_to videos_path
+  end
 
   private
   def video_params
